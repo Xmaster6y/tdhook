[BugFix] Fix gradient nested key issue (#19)
* issue nested

* better args exposition

* TODO

* modules_to_attribute not optional

src/tdhook/attribution/guided_backpropagation.py
@@ -2,11 +2,12 @@
 Guided Backpropagation
 """
 
-from typing import Callable, Tuple, Type
+from typing import Callable, Tuple, Type, Optional, List, Dict
 import torch
 from torch import nn
 from tensordict import TensorDict
 
+from tdhook._types import UnraveledKey
 from tdhook.modules import HookedModule
 from tdhook.hooks import MultiHookHandle, MultiHookManager, HookFactory, DIRECTION_TO_RETURN
 from tdhook.attribution.gradient_helpers import GradientAttribution
@@ -14,9 +15,36 @@ from tdhook.attribution.gradient_helpers import GradientAttribution
 
 class GuidedBackpropagation(GradientAttribution):
     def __init__(
-        self, *args, multiply_by_inputs: bool = False, classes_to_skip: Tuple[Type[nn.Module], ...] = (), **kwargs
+        self,
+        use_inputs: bool = True,
+        use_outputs: bool = True,
+        input_modules: Optional[List[str]] = None,
+        target_modules: Optional[List[str]] = None,
+        init_attr_targets: Optional[Callable[[TensorDict, TensorDict], TensorDict]] = None,
+        init_attr_inputs: Optional[Callable[[TensorDict, TensorDict], TensorDict]] = None,
+        init_attr_grads: Optional[Callable[[TensorDict, TensorDict], TensorDict]] = None,
+        additional_init_keys: Optional[List[UnraveledKey]] = None,
+        output_grad_callbacks: Optional[Dict[str, Callable]] = None,
+        attribution_key: UnraveledKey = "attr",
+        clean_intermediate_keys: bool = True,
+        cache_callback: Optional[Callable] = None,
+        multiply_by_inputs: bool = False,
+        classes_to_skip: Tuple[Type[nn.Module], ...] = (),
     ):
-        super().__init__(*args, **kwargs)
+        super().__init__(
+            use_inputs=use_inputs,
+            use_outputs=use_outputs,
+            input_modules=input_modules,
+            target_modules=target_modules,
+            init_attr_targets=init_attr_targets,
+            init_attr_inputs=init_attr_inputs,
+            init_attr_grads=init_attr_grads,
+            additional_init_keys=additional_init_keys,
+            output_grad_callbacks=output_grad_callbacks,
+            attribution_key=attribution_key,
+            clean_intermediate_keys=clean_intermediate_keys,
+            cache_callback=cache_callback,
+        )
         self._hook_manager = MultiHookManager(pattern=r".+", classes_to_skip=classes_to_skip)
         self._multiply_by_inputs = multiply_by_inputs
 

src/tdhook/attribution/lrp.py
@@ -2,7 +2,7 @@
 LRP
 """
 
-from typing import Callable, Optional, List
+from typing import Callable, Optional, List, Dict
 
 from torch import nn
 from warnings import warn
@@ -18,15 +18,37 @@ from tdhook.hooks import resolve_submodule_path
 class LRP(GradientAttribution):
     def __init__(
         self,
-        rule_mapper: Callable[[str, nn.Module], Rule | None],
+        use_inputs: bool = True,
+        use_outputs: bool = True,
+        input_modules: Optional[List[str]] = None,
+        target_modules: Optional[List[str]] = None,
+        init_attr_targets: Optional[Callable[[TensorDict, TensorDict], TensorDict]] = None,
+        init_attr_inputs: Optional[Callable[[TensorDict, TensorDict], TensorDict]] = None,
+        init_attr_grads: Optional[Callable[[TensorDict, TensorDict], TensorDict]] = None,
+        additional_init_keys: Optional[List[UnraveledKey]] = None,
+        output_grad_callbacks: Optional[Dict[str, Callable]] = None,
+        attribution_key: UnraveledKey = "attr",
+        clean_intermediate_keys: bool = True,
+        cache_callback: Optional[Callable] = None,
+        rule_mapper: Callable[[str, nn.Module], Rule | None] | None = None,
         warn_on_missing_rule: bool = True,
         skip_modules: Optional[Callable[[str, nn.Module], bool]] = None,
-        **kwargs,
     ):
         super().__init__(
-            **kwargs,
+            use_inputs=use_inputs,
+            use_outputs=use_outputs,
+            input_modules=input_modules,
+            target_modules=target_modules,
+            init_attr_targets=init_attr_targets,
+            init_attr_inputs=init_attr_inputs,
+            init_attr_grads=init_attr_grads,
+            additional_init_keys=additional_init_keys,
+            output_grad_callbacks=output_grad_callbacks,
+            attribution_key=attribution_key,
+            clean_intermediate_keys=clean_intermediate_keys,
+            cache_callback=cache_callback,
         )
-        self._rule_mapper = rule_mapper
+        self._rule_mapper = rule_mapper or (lambda name, module: None)
         self._warn_on_missing_rule = warn_on_missing_rule
         self._skip_modules = skip_modules
 