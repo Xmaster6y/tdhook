bec6ebf199834a588c355f5f2e14441c93df27b9
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2025-12-17 10:47:49 +0100

    tested version

--- src/tdhook/__init__.py
+++ src/tdhook/__init__.py
@@ -1,11 +1,7 @@
-"""
-tdhook
-"""
-
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("tdhook")
+    __version__ = version(__name__)
 except PackageNotFoundError:
     __version__ = "unknown"
 
