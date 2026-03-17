e057ce81494f3fbbde99ede9fd756f3072b85ca8
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2026-03-17 11:30:49 +0100

    desc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,8 @@ extensions = [
     "sphinx_design",  # Boostrap design components
     "nbsphinx",  # Jupyter notebook support
     "sphinxcontrib.bibtex",  # BibTeX citation support
-    "autoapi.extension",
-    "sphinx_llm.txt",
+    "autoapi.extension",  # Auto documentation from code
+    "sphinx_llm.txt",  # LLM support
 ]
 
 # BibTeX configuration
