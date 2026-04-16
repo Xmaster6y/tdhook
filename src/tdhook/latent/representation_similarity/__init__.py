f416f4c3a160e4e7b45f60360f8ce33c9c01682f
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2026-04-16 11:12:31 +0200

    Information imbalance for representation similarity (#49)
    * information imbalance
    
    * tests
    
    * docs
    
    * notebook run

--- src/tdhook/latent/representation_similarity/__init__.py
+++ src/tdhook/latent/representation_similarity/__init__.py
@@ -3,5 +3,6 @@ Representation similarity methods.
 """
 
 from .cka import CkaEstimator
+from .information_imbalance import InformationImbalanceEstimator
 
-__all__ = ["CkaEstimator"]
+__all__ = ["CkaEstimator", "InformationImbalanceEstimator"]
